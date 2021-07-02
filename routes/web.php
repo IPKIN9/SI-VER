9aed84b6cb06c1bddcb17a12060a4dd1df33c586
IPKIN9 <irwandirini@gmail.com>
2021-07-02 09:15:25 +0800

    Setup project done

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'Auth\AuthController@index')->name('login');
+Route::post('/login', 'Auth\AuthController@check')->name('auth.check');
+Route::get('/logout', 'Auth\AuthController@logout')->name('logout');
+Route::get('/register', 'Auth\AuthController@regist_view')->name('register.index');
+Route::post('/register_post', 'Auth\AuthController@register')->name('register.post');
+
+Route::prefix('dashboard')->group(function () {
+    Route::get('/dashboard', 'Dashboard\DsController@index')->name('ds.index');
 });
